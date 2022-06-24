5292b1ce731d34ce1fe3ee1faccce4b65bc4a904
Seunelvis <adeniranoluwaseunelvis@gmail.com>
2022-06-24 06:19:41 -0700

    Javascript Intro

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Hello world");
+
+var myName = "Adeniran Oluwaseun"
+var myHeight = "5.9 ft";
+var country = "Nigeria";
+
+document.write("My name is " + myName);
+document.write("<br>My height is " + myHeight);
+document.write("<br>My Country " + country);
+
